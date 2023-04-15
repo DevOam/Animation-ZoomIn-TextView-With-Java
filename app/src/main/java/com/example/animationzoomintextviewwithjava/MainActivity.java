2ee1178106e5af17dc42838b39af845ad3084471
DevOam <LAKOUAS-MOHAMEDREDA@ofppt-edu.ma>
2023-04-15 14:33:51 +0000

    Animation ZoomIn TextView With Java

---
+++ app/src/main/java/com/example/animationzoomintextviewwithjava/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.animationzoomintextviewwithjava;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    Animation anim;
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.text);
+
+
+        anim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.zoom_in);
+        text.setAnimation(anim);
+
+
+    }
+}
